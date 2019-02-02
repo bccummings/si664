109294147f00adf6de34304e693e29a2f47dbaa1
Brandon Cummings <cummingb@umich.edu>
2019-02-01 19:31:21 -0500

    section 4 challenge mode complete

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -21,7 +21,7 @@ admin.site.register(Book, BookAdmin)
 # register BookInstance
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-
+    list_display = ('book', 'status', 'due_back', 'id')
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
