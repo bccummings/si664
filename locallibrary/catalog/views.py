92b6add170182f9065dae3a090f32ef00cad112c
Brandon Cummings <cummingb@umich.edu>
2019-02-06 16:31:53 -0500

    part6 complete

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
-from catalog.models import Book, Author, BookInstance, Genre
 
 def index(request):
     """View function for home page of site."""
@@ -9,13 +10,13 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
+
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
+
+    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -25,3 +26,9 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
