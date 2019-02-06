83b01e119e35d0182dfe9745fb9e16f6f6ce32eb
Brandon Cummings <cummingb@umich.edu>
2019-02-06 17:47:54 -0500

    start part8

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -20,6 +20,7 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 #Add URL maps to redirect the base URL to our application
