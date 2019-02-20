098f9c8e2e7aa5ef9c3010a31e8487ec280f47ec
Brandon Cummings <cummingb@umich.edu>
2019-02-20 15:45:24 -0500

    update views and urls for dj4e example

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('autos/', include('autos.urls')'),
 ]
