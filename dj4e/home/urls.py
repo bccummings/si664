9333aa8da9898c3087acdc112818f445c34195c5
Brandon Cummings <cummingb@umich.edu>
2019-02-20 15:18:52 -0500

    start autos

--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),
 ]
