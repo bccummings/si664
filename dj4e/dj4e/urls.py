f57796abbd9e4f9f19a3157551d9534f1275b387
Brandon Cummings <cummingb@umich.edu>
2019-02-27 09:40:06 -0500

    finish cat assignment first try

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('autos/', include('autos.urls')),
+    #path('autos/', include('autos.urls')),
+    path('cats/', include('cats.urls')),
 ]
