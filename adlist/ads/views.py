b14fb909b748595f4ea0b837c3b55f3926e804d6
Brandon  Cummings <cummingb@Brandons-MacBook-Pro-2.local>
2019-04-14 14:48:35 -0400

    adjust adlist views

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,6 +1,5 @@
 # from django.views import View
-# from django.views import generic
-# from django.shortcuts import render
+from django.views import generic
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
