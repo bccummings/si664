96fb25b1cd84bc4ca167355debca713233a622e6
Brandon  Cummings <cummingb@Brandons-MacBook-Pro-2.local>
2019-04-14 14:50:20 -0400

    views was missing create form

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from ads.models import Ad
 from ads.util import AdListView, AdDetailView, AdCreateView, AdUpdateView, AdDeleteView
-from django.core.files.uploadedfile import InMemoryUploadedFile
+from ads.forms import CreateForm
 
 class AdListView(AdListView):
     model = Ad
