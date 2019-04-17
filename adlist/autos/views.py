40962b380764666dc62d8248caeeb79ab1a865a3
Brandon  Cummings <cummingb@Brandons-MacBook-Pro-2.local>
2019-04-17 17:15:25 -0400

    add views

--- adlist/autos/views.py
+++ adlist/autos/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from autos.models import Auto, Comment
+from autos.util import AutoListView, AutoDetailView, AutoCreateView, AutoUpdateView, AutoDeleteView
 
 class AutoListView(AutoListView):
     model = Auto
